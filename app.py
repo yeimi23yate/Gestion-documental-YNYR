17a7378ea51a5c4023d72be63b68fe6f51a715d1
yeimi23yate <yeimiyate@cbit-online.com>
2026-06-10 19:26:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ st.set_page_config(
     page_icon="📁",
     layout="wide"
 )
-col1, col2 = st.columns(2)
-
-with col1:
-    st.image("logo_azure.jpg", width=120)
-
-with col2:
-    st.image("log_CCB.png", width=120)
 st.sidebar.title("📁 Gestión Documental")
 
 menu = st.sidebar.selectbox(
