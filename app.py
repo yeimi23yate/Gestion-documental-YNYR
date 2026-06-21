8e7c01589d49bd49ab48d054ebb6e523386011d3
yeimi23yate <yeimiyate@cbit-online.com>
2026-06-20 20:15:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ st.markdown("""
 
 ### Centralice, controle y asegure la trazabilidad de la documentación de sus iniciativas.
 
-Centralice la documentación de sus iniciativas en una única plataforma, garantizando trazabilidad, control de versiones, aprobaciones formales y consulta eficiente de la información.
+Implemente un flujo estructurado de registro, revisión, aprobación y publicación que garantice la calidad documental, el control de cambios y la disponibilidad de información actualizada para todos los interesados.
 
 """)
 
